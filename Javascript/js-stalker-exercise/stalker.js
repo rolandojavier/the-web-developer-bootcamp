f29675758b682c9a824808fcba1853f693cd3aa8
Rolando Ramos <rjramos28@gmail.com>
2020-08-14 21:05:01 -0500

    JS Stalker Exercise

---
+++ Javascript/js-stalker-exercise/stalker.js
@@ -0,0 +1,6 @@
+const firstName = prompt('What is your first name?');
+const lastName = prompt('What is your last name?');
+const age = prompt('What is your age?');
+
+console.log('Your full name is ' + firstName + ' ' + lastName);
+console.log('You are ' + age + ' years old');
