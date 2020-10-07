1b8bb138f9a5340397a44c739acc515fccf8046b
Rolando Ramos <rjramos28@gmail.com>
2020-10-07 06:48:11 +0000

    YelpCamp - Creating campgrounds

--- Express.js/yelp-camp/app.js
+++ Express.js/yelp-camp/app.js
@@ -3,28 +3,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let campgrounds = [
+  {name: 'Volcan Baru', image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350'},
+  {name: 'Venta de Cruces', image: 'https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&h=350'},
+  {name: 'La Yeguada', image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350'},
+];
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    {
-      name: 'Volcan Baru',
-      image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350'
-    },
-    {
-      name: 'Venta de Cruces',
-      image: 'https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&h=350'
-    },
-    {
-      name: 'La Yeguada',
-      image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350'
-    },
-  ];
   res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', (req, res) => {
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {name: name, image: image}
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
 });
 
 app.listen(port, () => {
